2baf15865a12b2393cd57fb1dc81a5e836c8a52e
Ermolaev Aleksey (WORK) <ermolaev@spb.mmbank.ru>
2016-02-08 14:01:33 +0300

    Remove proxy list

--- lib/curl_query.php
+++ lib/curl_query.php
@@ -2,18 +2,6 @@
 
 function curl_get($url, $referer = 'http://www.google.com') {
 
-//$proxy = '117.136.234.8:81';
-//$proxy = '121.199.25.64:81';
-//$proxy = '141.101.118.191:80';
-
-
-//$proxy = '188.165.141.151:80';  	//Finland
-//$proxy = '46.37.193.74:8080';		//Ukraine
-//$proxy = '94.23.200.49:3128';		//France
-//$proxy = '86.57.177.11:1080';		//Belarus
-
-$proxy = '10.247.19.22:9090';
-$proxyauth = 'spb\eav:recf40vehf}|';
 
 $header  = array
 (
@@ -30,8 +18,8 @@ $header  = array
 	curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 6.1; rv:43.0) Gecko/20100101 Firefox/43.0");
 	curl_setopt($ch, CURLOPT_REFERER, $referer);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($ch, CURLOPT_PROXY, $proxy);
-	curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
+//	curl_setopt($ch, CURLOPT_PROXY, $proxy);
+//	curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
 //	curl_setopt($ch, CURLOPT_PROXYTYPE, CURLPROXY_SOCKS5);		//Использование прокси SOCKS5
 	
 	$data = curl_exec($ch);
@@ -43,8 +31,6 @@ $header  = array
 
 function get_ajax ($id) {
 
-$proxy = '10.247.19.22:9090';
-$proxyauth = 'spb\eav:recf40vehf}|';
 
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_URL, 'http://opt.eliseevaolesya.com/wp-content/themes/eliseeva%20olesya/cart.php');
@@ -56,8 +42,8 @@ $proxyauth = 'spb\eav:recf40vehf}|';
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	curl_setopt($ch, CURLOPT_POST, true);
 	curl_setopt($ch, CURLOPT_POSTFIELDS, "id=".$id);
-	curl_setopt($ch, CURLOPT_PROXY, $proxy);
-	curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
+//	curl_setopt($ch, CURLOPT_PROXY, $proxy);
+//	curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
 	$data = curl_exec($ch);
 	curl_close($ch);
 	return $data;
